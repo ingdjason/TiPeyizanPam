7a90105d34842de4ba127ff7b767dd8e1c3bfe4d
Djason irvinton SYLVAINCE <djasoirvinton.sylvaince@esih.edu>
2015-08-28 03:23:48 -0400

    app

--- app/src/main/java/com/djamoli/tipeyizanpam/ActivityTwo.java
+++ app/src/main/java/com/djamoli/tipeyizanpam/ActivityTwo.java
@@ -1,16 +1,31 @@
 package com.djamoli.tipeyizanpam;
 
 import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 
 
 public class ActivityTwo extends ActionBarActivity {
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private ViewFlipper mViewFlipper;
+    private Context mContext;
+    private final GestureDetector detector = new GestureDetector(new SwipeGestureDetector());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,63 @@ public class ActivityTwo extends ActionBarActivity {
         getSupportActionBar().setLogo(R.mipmap.ic_logo);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_two);
+
+        mContext = this;
+        mViewFlipper = (ViewFlipper) this.findViewById(R.id.view_flipper);
+        mViewFlipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(final View view, final MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
+
+        ImageView imageView = new ImageView(this);
+        //imageView.setImageResource(R.drawable.fakeslide);
+        mViewFlipper.addView(imageView);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sets auto flipping
+                mViewFlipper.setAutoStart(true);
+                mViewFlipper.setFlipInterval(4000);
+                mViewFlipper.startFlipping();
+            }
+        });
+
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //stop auto flipping
+                mViewFlipper.stopFlipping();
+            }
+        });
+
+        class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                try {
+                    // right to left swipe
+                    if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_in));
+                        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_out));
+                        mViewFlipper.showNext();
+                        return true;
+                    } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.right_in));
+                        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext,R.anim.right_out));
+                        mViewFlipper.showPrevious();
+                        return true;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        }
         //ImageView gif = (ImageView) findViewById(R.id.imageView15);
         //Ion.with(gif).load("R.drawable.slide");
         configureButton();
---
+++ app/src/main/java/com/djamoli/tipeyizanpam/SimpleOnGestureListener.java
@@ -0,0 +1,7 @@
+package com.djamoli.tipeyizanpam;
+
+/**
+ * Created by Ing Djason(Admin) on 27-Aug-15.
+ */
+public class SimpleOnGestureListener {
+}
